3e5a60f6d8e788423da1382d1be08dc175e43dfb
dat dang <2imdanton139@gmail.com>
2026-07-16 01:55:27 +0700

    Build Economic Shieldwall: update test/realtime.test.js

--- test/realtime.test.js
+++ test/realtime.test.js
@@ -68,8 +68,8 @@ test("host tạo phòng và bắt đầu realtime chỉ với một đại diệ
   const state = await playingState;
   assert.equal(state.playersCount, 1);
   assert.ok(state.game);
-  assert.ok(state.game.riders[joined1.teamId]);
-  assert.equal(state.objective.kind, "pickup");
+  assert.ok(state.game.commanders[joined1.teamId]);
+  assert.equal(state.game.soldiers[joined1.teamId].length, 5);
 });
 
 test("phòng nhận đủ 8 đại diện, mỗi đội một người và chặn người thứ 9", async (t) => {
